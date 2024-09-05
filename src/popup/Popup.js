514ceae7aae8509c36c0c9b626a000c5234fb80b
abhaysharma2719 <abhay@techcrumb.com>
2024-09-06 00:30:51 +0530

    initial commit

---
+++ src/popup/Popup.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Popup() {
+    return (
+        <div className="p-4 h-[400px] w-[400px]">
+            <h1 className="text-xl font-bold">My Chrome Extension</h1>
+            <p>Welcome to the popup!</p>
+        </div>
+    );
+}
+
+export default Popup
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: {
+        background : './src/background.js',
+        popup: './src/index.js', // React popup
+        content: './src/content.js', // Content script
+    },
+    output: {
+        filename: '[name].js', // Name based on entry (popup.js, content.js, etc.)
+        path: path.resolve(__dirname, 'extension'), // Output directory
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader', 'postcss-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/popup/popup.html',
+            filename: 'popup.html',
+            chunks: ['popup'] // Only include popup.js in popup.html
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'public/manifest.json', to: '' }, // Copy manifest.json
+            ]
+        })
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx']
+    }
+};
